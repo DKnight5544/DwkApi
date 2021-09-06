d455143f6daf1e2df6dc88ebf6b035b49788c00f
DKnight5544 <dave@mostlyspam.com>
2021-09-06 10:12:42 -0700

    Add project files.

---
+++ Controllers/TimersController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DwkApi.Controllers
+{
+    public class TimersController : ApiController
+    {
+
+
+        [Route("api/timers/GetNewPage")]
+        [HttpGet]
+        public TimerPage[] GetNewPage()
+        {
+            string connStr = Environment.GetEnvironmentVariable("DWKDBConnectionString");
+            using (DWKDBDataContext db = new DWKDBDataContext(connStr))
+            {
+                return db.InsertNewPage().ToArray();
+            }
+        }
+
+
+        [Route("api/timers/GetAllByPage/{id}")]
+        [HttpGet]
+        public TimerPage[] GetAllByPage(string id)
+        {
+            string connStr = Environment.GetEnvironmentVariable("DWKDBConnectionString");
+            using (DWKDBDataContext db = new DWKDBDataContext(connStr))
+            {
+                return db.SelectAllByPage(id).ToArray();
+            }
+        }
+
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DwkApi.WebApiApplication" Language="C#" %>
