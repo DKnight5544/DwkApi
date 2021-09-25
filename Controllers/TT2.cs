b8c35c6aa4dfc3eef87ad372930a504e51359925
DKnight5544 <dave@mostlyspam.com>
2021-09-25 15:20:19 -0700

    Adding TT2

---
+++ Controllers/TT2.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Linq;
+using System.Web.Http;
+using DwkApi.TT2.Models;
+
+namespace DwkApi.Controllers
+{
+    public class TT2Controller : ApiController
+    {
+
+
+        [Route("api/TT2/AddRootTimer")]
+        [HttpGet]
+        public TimerModel AddRootTimer()
+        {
+            string connStr = Environment.GetEnvironmentVariable("DWKDBConnectionString");
+            using (TT2DataContext tt2 = new TT2DataContext())
+            {
+                return tt2.AddRootTimer().SingleOrDefault();
+            }
+        }
+
+        [Route("api/TT2/AddTimer/{id}")]
+        [HttpGet]
+        public TimerModel AddTimer(string id)
+        {
+            string connStr = Environment.GetEnvironmentVariable("DWKDBConnectionString");
+            using (TT2DataContext tt2 = new TT2DataContext())
+            {
+                return tt2.AddTimer(id).SingleOrDefault();
+            }
+        }
+
+        [Route("api/TT2/GetTimer/{id}")]
+        [HttpGet]
+        public TimerModel GetTimer(string id)
+        {
+            string connStr = Environment.GetEnvironmentVariable("DWKDBConnectionString");
+            using (TT2DataContext tt2 = new TT2DataContext())
+            {
+                return tt2.GetTimer(id).SingleOrDefault();
+            }
+        }
+
+        [Route("api/TT2/GetChildren/{id}")]
+        [HttpGet]
+        public TimerModel[] GetChildren(string id)
+        {
+            string connStr = Environment.GetEnvironmentVariable("DWKDBConnectionString");
+            using (TT2DataContext tt2 = new TT2DataContext())
+            {
+                return tt2.GetChildren(id).ToArray();
+            }
+        }
+
+
+        [Route("api/TT2/ToggleTimer/{id}")]
+        [HttpGet]
+        public TimerModel ToggleTimer(string id)
+        {
+            string connStr = Environment.GetEnvironmentVariable("DWKDBConnectionString");
+            using (TT2DataContext tt2 = new TT2DataContext())
+            {
+                return tt2.ToggleTimer(id).SingleOrDefault();
+            }
+        }
+
+        [Route("api/TT2/AdjustTimer/{id}")]
+        [HttpPost]
+        public TimerModel AdjustTimer(AdjustParms parms)
+        {
+            string connStr = Environment.GetEnvironmentVariable("DWKDBConnectionString");
+            using (TT2DataContext tt2 = new TT2DataContext())
+            {
+                return tt2.AdjustTimer(parms.TimerKey, parms.SecondsOffset).SingleOrDefault();
+            }
+        }
+
+        //[Route("api/TT2/DeleteTimer/{id}")]
+        //[HttpGet]
+        //public void AdjustTimer(AdjustParms parms)
+        //{
+        //    string connStr = Environment.GetEnvironmentVariable("DWKDBConnectionString");
+        //    using (TT2DataContext tt2 = new TT2DataContext())
+        //    {
+        //        return tt2.AdjustTimer(parms.TimerKey, parms.SecondsOffset).SingleOrDefault();
+        //    }
+        //}
+
+
+
+
+
+    }
+}
---
+++ Models/AdjustParms.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DwkApi.TT2.Models
+{
+    public class AdjustParms
+    {
+        public string TimerKey { get; set; }
+        public int SecondsOffset { get; set; }
+    }
+}
